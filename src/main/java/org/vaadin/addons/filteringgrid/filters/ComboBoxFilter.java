df205a4f58920c52439280883fcb5014e66c198f
Adam Wagner <adam@vaadin.com>
2018-01-26 11:50:55 +0200

    Add combo box filter

---
+++ src/main/java/org/vaadin/addons/filteringgrid/filters/ComboBoxFilter.java
@@ -0,0 +1,35 @@
+package org.vaadin.addons.filteringgrid.filters;
+
+import java.util.Collection;
+
+import com.vaadin.data.ValueProvider;
+import com.vaadin.server.SerializableBiPredicate;
+import com.vaadin.ui.ComboBox;
+
+public class ComboBoxFilter<T, V> extends
+        InMemoryFilterComponentWrapper<T, V, V, ComboBox<V>> {
+
+    private final ValueProvider<T, V> valueProvider;
+    private final SerializableBiPredicate<V, V> filterPredicate;
+
+    public ComboBoxFilter(Collection<V> items, Class<V> type,
+            ValueProvider<T, V> valueProvider,
+            SerializableBiPredicate<V, V> filterPredicate) {
+        super(new ComboBox<>(), type);
+
+        this.valueProvider = valueProvider;
+        this.filterPredicate = filterPredicate;
+
+        getWrappedComponent().setItems(items);
+    }
+
+    @Override
+    public ValueProvider<T, V> getValueProvider() {
+        return valueProvider;
+    }
+
+    @Override
+    public SerializableBiPredicate<V, V> getFilterPredicate() {
+        return filterPredicate;
+    }
+}
--- src/test/java/org/vaadin/addons/filteringgrid/FilterGridTest.java
+++ src/test/java/org/vaadin/addons/filteringgrid/FilterGridTest.java
@@ -1,8 +1,14 @@
 package org.vaadin.addons.filteringgrid;
 
+import java.util.Arrays;
+
 import org.vaadin.addonhelpers.AbstractTest;
 import org.vaadin.addons.filteringgrid.data.Person;
+import org.vaadin.addons.filteringgrid.data.Person.Continent;
 import org.vaadin.addons.filteringgrid.data.PersonService;
+import org.vaadin.addons.filteringgrid.filters.ComboBoxFilter;
+import org.vaadin.addons.filteringgrid.filters.InMemoryFilter.Comparator;
+import org.vaadin.addons.filteringgrid.filters.NumberFilter;
 import org.vaadin.addons.filteringgrid.filters.TextFilter;
 
 import com.vaadin.data.provider.DataProvider;
@@ -14,8 +20,31 @@ public class FilterGridTest extends AbstractTest {
     @Override
     public Component getTestComponent() {
         FilterGrid<Person> grid = new FilterGrid<>(Person.class);
+
         grid.getColumn("firstName")
                 .setFilter(new TextFilter<>(Person::getFirstName));
+
+//        grid.getColumn("balance").setFilter(InMemoryFilter
+//                .wrapComponent(new TextField(), String.class,
+//                        Person::getBalance, (value, filterValue) -> {
+//                            Float filterValueFloat = null;
+//                            try {
+//                                filterValueFloat = Float.valueOf(filterValue);
+//                            } catch (NumberFormatException e) {
+//
+//                            }
+//                            return filterValueFloat != null ?
+//                                    value.compareTo(filterValueFloat) < 0
+//                                    : true;
+//                        }));
+        grid.getColumn("balance").setFilter(NumberFilter
+                .createFloatFilter(Person::getBalance,
+                        Comparator.smallerThan()));
+
+        grid.getColumn("continent").setFilter(
+                new ComboBoxFilter<>(Arrays.asList(Continent.values()),
+                        Continent.class, Person::getContinent,
+                        (v, f) -> f == null || f.equals(v)));
 
         grid.setFilteredDataProvider(DataProvider
                 .ofCollection(PersonService.getInstance().getAll()));
